3d74e71c61ddae85ba67d3aaa7746ab0473acd70
89882 <pgh123166@naver.com>
2023-10-11 14:48:15 +0900

    feat: dom 및 event 코드 생성

---
+++ src/core/Dom.js
@@ -0,0 +1,54 @@
+import { updateElement } from './diff';
+
+let oldComponent;
+
+export function enrollEvent(component) {
+  component.setEvent();
+  component.children.map((child) => {
+    child.setEvent();
+  });
+}
+
+export function clearEvent(component) {
+  component.clearEvent();
+  component.children.map((child) => {
+    child.clearEvent();
+  });
+}
+
+export function updateDom(component) {
+  const { componentRoot } = component;
+  clearEvent(component);
+
+  const newNode = componentRoot.cloneNode(true);
+  newNode.innerHTML = component.template();
+  const oldChildNodes = [...componentRoot.childNodes];
+  const newChildNodes = [...newNode.childNodes];
+  const max = Math.max(oldChildNodes.length, newChildNodes.length);
+
+  for (let i = 0; i < max; i++) {
+    updateElement(componentRoot, newChildNodes[i], oldChildNodes[i]);
+  }
+  enrollEvent(component);
+} // 리렌더링
+
+export function createDom(component) {
+  const { componentRoot } = component;
+
+  if (oldComponent) {
+    clearEvent(oldComponent);
+  }
+  oldComponent = component;
+
+  const newNode = componentRoot.cloneNode(true);
+  newNode.innerHTML = component.template();
+  const oldChildNodes = [...componentRoot.childNodes];
+  const newChildNodes = [...newNode.childNodes];
+
+  const max = Math.max(oldChildNodes.length, newChildNodes.length);
+
+  for (let i = 0; i < max; i++) {
+    updateElement(componentRoot, newChildNodes[i], oldChildNodes[i]);
+  }
+  enrollEvent(component);
+} // 렌더링
