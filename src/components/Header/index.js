f6dae6f70932192008f10cab07d6b43e8e416c28
89882 <pgh123166@naver.com>
2023-10-13 22:17:45 +0900

    feat: 로그인 구현

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,45 @@
 import { Component } from '../../core/Component';
 import logo from '../../assets/logo.png';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../../apis/firebase';
+import { userStore } from '../../store/user';
+import { navigate } from '../../core/router';
 
 export default class Header extends Component {
+  constructor(root) {
+    super(root);
+    userStore.subscribe('user', () => {
+      this.render();
+    });
+  }
   template() {
+    const user = userStore.state.user;
     return `<header class="max-w-[81.25rem] mt-[1.6875rem] mx-auto">
                 <section class="flex justify-between items-center">
                   <div class="flex items-center gap-[0.6875rem]">
                     <img src="${logo}" alt="logo" class="w-[2.7rem] h-[2.5rem] cursor-pointer">
                     <div class="text-3xl font-bold text-primary">shareIT</div>
                   </div>
-                  <button class="text-xl font-normal text-gray" id="loginButton">로그인</button>
+                  ${
+                    user === ''
+                      ? ''
+                      : user === null
+                      ? '<button class="text-xl font-normal text-gray" id="loginButton">로그인</button>'
+                      : `<button class="text-xl font-normal text-blue" id="navigateMy">
+                          ${user.displayName}님
+                        </button>`
+                  }
                 </section>
             </header>`;
   }
   setEvent() {
-    this.addEvent('click', '#loginButton', () => {
-      console.log('click');
+    const provider = new GoogleAuthProvider();
+    this.addEvent('click', '#loginButton', async () => {
+      await signInWithPopup(auth, provider);
+    });
+
+    this.addEvent('click', '#navigateMy', () => {
+      navigate('/my');
     });
   }
 }
