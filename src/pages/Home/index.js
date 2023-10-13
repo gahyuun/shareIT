f2919524aae9ccdf19dc89a31568d5c9ba1a47ba
89882 <pgh123166@naver.com>
2023-10-13 23:33:33 +0900

    feat: navigate write 버튼 구현

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import { Component } from '../../core/Component';
+import { navigate } from '../../core/router';
 
 export default class Home extends Component {
   template() {
@@ -7,8 +8,15 @@ export default class Home extends Component {
               <li class="text-5xl font-bold text-primary">shareIT</li>
               <li class="text-2xl font-semibold text-primary mt-[0.75rem]">개발자 아티클 공유 서비스</li>
               <li class="text-xl font-normal text-gray mt-[1.6875rem]">아티클과 아티클에 대한 당신의 생각을 공유해주세요!</li>
-              <button class="bg-blue text-white w-[8rem] h-[2.3125rem] px-[0.5rem] rounded-md mt-8">공유하러 가기</button>
+              <button class="bg-blue text-white w-[8rem] h-[2.3125rem] px-[0.5rem] rounded-md mt-8" id="navigateWrite">
+              공유하러 가기
+              </button>
               </ul>
             </nav>`;
+  }
+  setEvent() {
+    this.addEvent('click', '#navigateWrite', () => {
+      navigate('/write');
+    });
   }
 }
