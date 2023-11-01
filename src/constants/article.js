fc34ac654c6163bc4b3202a8d3fccc15623b06ef
89882 <pgh123166@naver.com>
2023-11-01 23:00:30 +0900

    chore: MAX_ARTICLES_LENGTH 상수 값 12-> 16

--- src/constants/article.js
+++ src/constants/article.js
@@ -6,6 +6,6 @@ export const START_INDEX = 0;
 
 export const MAX_CONTENT_LENGTH = 70;
 
-export const MAX_ARTICLES_LENGTH = 12;
+export const MAX_ARTICLES_LENGTH = 16;
 
 export const MAX_USER_ARTICLES_LENGTH = 6;
