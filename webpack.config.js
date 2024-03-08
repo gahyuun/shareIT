75d4b61c0fd2e8d67010f79b9e61fc88c80e2304
gahyuun <pgh123166@naver.com>
2024-03-08 18:20:38 +0900

    chore: output path 변경

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   // 웹팩 작업을 통해 생성된 결과물
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'output'),
     filename: '[name][contenthash].js',
     // clean: true,
     assetModuleFilename: '[name][ext]',
